a15009f0867c50a02ef48bcc24a50f01435afd10
apikkondang <apikkondang@gmail.com>
2021-07-01 03:03:22 +0700

    Add files via upload

---
+++ lang-afparadise.js
@@ -0,0 +1,58 @@
+let urlParamsObject = new URLSearchParams(window.location.search)
+let urlParamsString = urlParamsObject.toString() 
+    let lang = urlParamsObject.get('lang')
+	var bhs = lang;
+	
+        if(bhs == "ID")         var btn1 = "Coba Sekarang"   
+        else                    var btn1 = "Get Started"    
+
+
+        if(bhs == "ID")         var P1 = "Memiliki macam jenis offer, mencangkup semua negara, dan pembayaran mingguan."   
+        else                    var P1 = "has so many offers, including all country, and weekly paid"    
+
+
+        if(bhs == "ID")         var P2 = "Pembayaran rutin setiap minggu"
+        else                    var P2 = "Paid every single week"    
+        	if(bhs == "ID")         var P2b = "Kami akan membayar anda setiap minggu pada hari selasa. Kami akan membayar tanpa ada minimal saldo balance."
+        	else                    var P2b = "You will be get you’re payment every single week on Tuesday. We will paid with no minimum balance."
+        	if(bhs == "ID")         var P2c = "Mendukung pembayaran"
+        	else                    var P2c = "Support at"
+
+
+        if(bhs == "ID")         var P3 = "Affiliate manager bersidia memandu Anda 7x24 jam."
+        else                    var P3 = "Affiliate manager will stand by to guide you 7x24 hours."
+        	if(bhs == "ID")         var P3b = "Affiliate manager akan selalu bersedia memandu Anda, walaupun anda masih pemula sekalipun."
+        	else                    var P3b = "Affiliate manager will always ready to give you a guidance, even if you are beginner."
+
+
+        if(bhs == "ID")         var P4 = "Gratis Study Case"
+        else                    var P4 = "Free Study Case"
+        	if(bhs == "ID")         var P4b = "Kami akan memberi study case gratis setiap kali kami mendapatkan wining campaign kita akan memberitahu Anda. Dengan ini anda dapat mengambil keuntungan dan profit lebih banyak."
+        	else                    var P4b = "We will give a free study case every we have a winning campaign and notify it to you. With this way you will get a lot of benefit and profit."
+        	
+        	if(bhs == "ID")         var P4c = "LP dan PLP"
+        	else                    var P4c = "LP and PLP"
+        		if(bhs == "ID")         var P4ca = "Memberi tahu LP & PLP wining untuk kamu"
+        		else                    var P4ca = "Inform you LP & PLP winning for you"
+        	if(bhs == "ID")         var P4d = "Target Ads"
+        	else                    var P4d = "Target Ads"
+        		if(bhs == "ID")         var P4da = "Target ads dari umur beserta lokasi kota terbaik"
+        		else                    var P4da = "target ads from the best ages and location "
+        	if(bhs == "ID")         var P4e = "Artikel Blog Study Case"
+        	else                    var P4e = "Article Blog Study Case"
+        		if(bhs == "ID")         var P4ea = "Membuatkan blog bacaan mengenai study case"
+        		else                    var P4ea = "Create article blog about study case "
+        	if(bhs == "ID")         var P4f = "Contoh Adscopy"
+        	else                    var P4f = "Samples ads"
+        		if(bhs == "ID")         var P4fa = "Memberikan Contoh adscopy"
+        		else                    var P4fa = "Give you example adscopy"
+
+
+        if(bhs == "ID")         var P5 = "Custom Webcopy"
+        else                    var P5 = "Custom Webcopy"
+        	if(bhs == "ID")         var P5b = "Jika Anda memiliki webcopy atau ide sendiri, kami dapat membuatkan LP atau PLP private khusus Anda, teknisi kami akan selalu siap membantu Anda."
+        	else                    var P5b = "if you have “webcopy” or a idea by your self, we can create LP or PLP custom private for you, we will always ready to help you"
+
+
+        if(bhs == "ID")         var P6 = "Hubungi Kami"
+        else                    var P6 = "Contact Us"
